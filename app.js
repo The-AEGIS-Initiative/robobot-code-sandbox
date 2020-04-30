d24dd92d59ff3ebcb6b96f3a52437bde86d7bfd2
Kevin <kevin.qi@berkeley.edu>
2020-04-30 16:28:56 -0700

    allow all cors origins

--- app.js
+++ app.js
@@ -49,12 +49,12 @@ app.use(cors({
   origin: function(origin, callback){
     // allow requests with no origin 
     // (like mobile apps or curl requests)
-    return callback(null, true);
+
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
       var msg = 'The CORS policy for this site does not ' +
                 'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
+      //return callback(new Error(msg), false);
     }
     return callback(null, true);
   }
