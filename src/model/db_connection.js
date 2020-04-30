3a4c9d3d303243ad8f60d7b3c8bb108c98d49c29
Kevin <kevin.qi@berkeley.edu>
2020-04-30 16:02:23 -0700

    Set replicaSet to Robobot-shard-00

--- src/model/db_connection.js
+++ src/model/db_connection.js
@@ -15,12 +15,12 @@ if(process.env.NODE_ENV=='development') { // Dev environment
 	const DB_USERNAME = process.env.DB_USERNAME
 	const DB_PASSWORD = process.env.DB_PASSWORD
 	const DB_URL = process.env.DB_URL
-	server = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}${DB_URL}/robobot-database?ssl=true&replicaSet=robobot-database`;
+	server = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}${DB_URL}/robobot-database?ssl=true&replicaSet=Robobot-shard-0`;
 }
 
 
 // Connect to MongoDB
 console.log(server)
-mongoose.connect(server, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+mongoose.connect(server, { useNewUrlParser: true, useCreateIndex: true })
 				.then(() => console.log('Atlas MongoDB connection succeeded'))
 				.catch(err => console.log(err));
